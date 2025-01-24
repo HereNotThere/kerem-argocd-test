56cd3dadc36fc2dbedb324fdec4a6722ab533521
Kerem Kazan <kerem.kazan@gmail.com>
2025-01-24 11:12:52 -0500

    adding apps

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,20 @@
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm repo update
 
-helm install argocd argo/argo-cd \
-  --version 7.7.16 \
-  --namespace argocd \
-  --set server.ingress.enabled=true \
-  --set server.service.type=ClusterIP \
-  --create-namespace
+# helm install argocd argo/argo-cd \
+#   --version 7.7.16 \
+#   --namespace argocd \
+#   --set server.ingress.enabled=true \
+#   --set server.service.type=ClusterIP \
+#   --create-namespace
 
-# TODO: maybe all of these should also be a separate helm chart that describe underlying resources for the entire project
+# # TODO: maybe all of these should also be a separate helm chart that describe underlying resources for the entire project
 
+
+
+argocd app create apps \
+    --dest-namespace argocd \
+    --dest-server https://kubernetes.default.svc \
+    --repo https://github.com/HereNotThere/kerem-argocd-test \
+    --path apps  
+argocd app sync apps 
