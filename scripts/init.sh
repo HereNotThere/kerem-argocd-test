2c16835bedab4bc76fb3250feaedd77c7f58f3c0
Kerem Kazan <kerem.kazan@gmail.com>
2025-01-25 15:56:56 -0500

    update external secrets chart source

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,24 +1,6 @@
-# helm repo add argo https://argoproj.github.io/argo-helm
-# helm repo update
-# helm install argocd argo/argo-cd \
-#   --version 7.7.16 \
-#   --namespace argocd \
-#   --set server.ingress.enabled=true \
-#   --set server.service.type=ClusterIP \
-#   --create-namespace
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add external-secrets https://charts.external-secrets.io
+helm repo update
 
-# # # TODO: make this configurable, or better yet turn this whole declaration into a separate argo-cd app
-# argocd app create app-of-apps \
-#     --dest-namespace argocd \
-#     --dest-server https://kubernetes.default.svc \
-#     --repo https://github.com/HereNotThere/kerem-argocd-test \
-#     --path charts/app-of-apps \
-#     --values ../../environments/gamma/values.yaml \
-#     --sync-policy automated \
-#     --auto-prune=true \
-#     --self-heal=true \
-#     --upsert
-    
-# # argocd app sync app-of-apps
-
+# TODO: don't hardcode the environment name
 kubectl apply -f ./environments/gamma/applications.yaml
