a0f12f33585ab46f8a15c22862a9c61390775f5f
Kerem Kazan <kerem.kazan@gmail.com>
2025-01-28 09:23:04 -0500

    - -> _

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add external-secrets https://charts.external-secrets.io
 helm repo update
 
-helm install argocd argo/argo-cd -n argocd
+helm install argocd argo/argo-cd -n argocd --create-namespace
 
 # TODO: don't hardcode the environment name
 kubectl apply -f ./environments/gamma/applications.yaml
