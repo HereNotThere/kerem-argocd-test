ec61b7410c2bd70727c9d3c2f861d668276ada6d
Kerem Kazan <kerem.kazan@gmail.com>
2025-01-30 15:55:20 -0500

    cert-manager wip

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,7 @@ fi
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add external-secrets https://charts.external-secrets.io
+helm repo add jetstack https://charts.jetstack.io 
 helm repo update
 
 helm install argocd argo/argo-cd -n argocd --create-namespace
